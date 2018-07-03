b39eae195e88cba53cd5650a626e5683443b223c
unknown <hzjio@outlook.com>
2018-07-04 01:58:31 +0800

    test

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,7 @@
 // const webapck = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 const helper = require('./util');
 
